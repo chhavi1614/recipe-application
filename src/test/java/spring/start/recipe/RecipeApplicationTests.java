ae61a189b8e278bdae41686fa04044b3db71d7ef
chhavi1614 <chhavi.jain.mat17@itbhu.ac.in>
2020-10-07 21:04:51 +0530

    Initial commit

---
+++ src/test/java/spring/start/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.start.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
